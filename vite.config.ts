b3932a69c6797cbd08f6c38ed9e280fa71e40288
wuhan <912056724@qq.com>
2023-01-31 18:48:54 +0800

    提交demo分支

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,29 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { babel } from '@rollup/plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  esbuild: {
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
+  },
+  plugins: [
+    babel({
+      plugins: [
+        [
+          '@babel/plugin-transform-react-jsx',
+          {
+            runtime: 'automatic',
+            importSource: '@antv/f2',
+          },
+        ],
+      ],
+    }),
+    vue(),
+    vueJsx(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
