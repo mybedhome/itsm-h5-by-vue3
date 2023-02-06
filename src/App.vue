5bea83e51817bce6e6de1b4fafa20889478c26c9
wuhan <912056724@qq.com>
2023-02-06 16:34:35 +0800

    add git-spec.md

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,11 @@ import { themeVars } from './constants/theme-vars';
 </script>
 
 <template>
-  <div id="root">
-    <van-config-provider :theme-vars="themeVars">
-      <router-view v-slot="{ Component }">
-        <component :is="Component" :key="$route.path" />
-      </router-view>
-    </van-config-provider>
-  </div>
+  <van-config-provider :theme-vars="themeVars">
+    <router-view v-slot="{ Component }">
+      <component :is="Component" :key="$route.path" />
+    </router-view>
+  </van-config-provider>
 </template>
 
-<style scoped>
-#root {
-  width: 100%;
-  height: 100vh;
-  background-color: #ecedee;
-}
-</style>
+<style scoped></style>
