96ad082f096e2fc31d856254fe11f9a4b1934b1e
wuhan <912056724@qq.com>
2023-03-25 18:07:35 +0800

    修复oauth页面bug

--- public/config.js
+++ public/config.js
@@ -6,7 +6,7 @@ window.g = {
   // 应用ID标识
   CLIENT_ID: '',
   // 移动端地址，默认是根路径, 如果部署在非根路径要加上二级路径地址 如: http://192.168.0.1:8000/itsm
-  OAUTH_MOBILE_URL: '/',
+  OAUTH_MOBILE_URL: location.origin, // 二级路径地址 location.origin + '/itsm'
   // PC页面地址（可选），如果部署在非根路径要加上二级路径地址，如: http://192.168.0.1:8001/itsm
   OAUTH_PC_URL: '',
 };
--- src/views/oauth/oauth.vue
+++ src/views/oauth/oauth.vue
@@ -2,25 +2,29 @@
 import { utils } from '@/utils';
 import { useRoute } from 'vue-router';
 
-const isMobile = utils.platform().isMobile;
-
 // 接收后端传过来的参数
 const query = new URLSearchParams(
   useRoute().query as unknown as string
 ).toString();
+const queryParams = utils.isEmptyObject(query) ? '' : '?' + query;
 
 // TODO: 免登陆后在钉钉或微信打开的移动端页面地址, 例如: /#/mobile/orderDetail
-const mobileUrl = '';
+const mobileUrl = '/#/bes/orders/add';
 // TODO: 免登陆后在钉钉或微信打开的PC端页面地址, 例如: /#/pc/orderDetail
 const webUrl = '';
 
 const OAUTH_MOBILE_URL = window.g.OAUTH_MOBILE_URL.replace(/\/$/, '');
 const OAUTH_PC_URL = window.g.OAUTH_PC_URL.replace(/\/$/, '');
-if (isMobile) {
-  location.href = `${OAUTH_MOBILE_URL}${mobileUrl}?${query}`;
-  return;
-}
-if (OAUTH_PC_URL) {
-  location.href = `${OAUTH_PC_URL}${webUrl}?${query}`;
+
+const prefix = `${window.g.UM_URL}?clientId=${window.g.CLIENT_ID}&redirectUrl=`;
+
+if (utils.platform().isMobile) {
+  location.href = `${prefix}${encodeURIComponent(
+    OAUTH_MOBILE_URL + mobileUrl + queryParams
+  )}`;
+} else if (OAUTH_PC_URL) {
+  location.href = `${prefix}${encodeURIComponent(
+    OAUTH_PC_URL + webUrl + queryParams
+  )}`;
 }
 </script>
